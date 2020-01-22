676cbc777fd67b8a1cac6df38260c8d3df631dfb
Udayskai <udayskai@gmail.com>
2020-01-23 01:37:43 +0530

    joi validation pending

--- Task2_express_mongoDb_Interface/routes/index.js
+++ Task2_express_mongoDb_Interface/routes/index.js
@@ -3,19 +3,17 @@ let Data=require('../dbModel/index.js');
 let router = express.Router();
 
 
-
+//post genre
 router.post("/postgenre", async(req,res)=>{
-   
+    
     let newData = new Data.GenreModel({
-        genre:req.body.genre
-    })
-   let data = await newData.save();
-   
-   res.send({message:"genre is added",d: await Data.GenreModel.find()})
+        genre:req.body.genre  })
+     let data = await newData.save();
+     res.send({message:"genre is added",d: await Data.GenreModel.find()})
 })
 
 
-
+//post movie with genre Id
 router.post("/postmovie",async(req,res)=>{
     let Movie =  new Data.MovieModel({
         name:req.body.name,
@@ -28,21 +26,33 @@ router.post("/postmovie",async(req,res)=>{
 })
 
 
+//get genre
 router.get("/getgenre",async(req,res)=>{
     let Output = await Data.GenreModel.find()
     res.send(Output)
 })
 
 
+//get movie
 router.get('/getmovie',async(req,res)=>{
     let Output=await Data.MovieModel.find().populate("genreId")
     res.send(Output);
 })
 
+
+//get by id
 router.get('/getmovie/:id',async(req,res)=>{
     let Output=await Data.MovieModel.findById(req.params.id).populate("genreId")
     res.send(Output);
 })
+
+
+// function Movievalidation(error){
+//     let schema = joy.object({
+//     name:joy.String().Required(),
+//})
+
+// }
 
   
 module.exports=router;
